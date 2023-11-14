7ea7191e0f4f11245a2855251302b0bc6e08739d
Pushkar Bankar <102483187+thepushkarB@users.noreply.github.com>
2023-11-14 17:09:01 +0530

    old code removed

--- memeGenerator/memeGeneratorScript.js
+++ memeGenerator/memeGeneratorScript.js
@@ -1,20 +1,13 @@
-let generateMemeBtn = document.querySelector(".generateBtn");
+̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨̨let generateMemeBtn = document.querySelector(".generateBtn");
 let memeTitleText = document.querySelector(".memeTitle");
 let memeImg = document.querySelector(".mainDiv img");
 let memeAuthorText = document.querySelector(".authorName");
 
 
 // update
-// function updateDetails(title, url, author) {
-//     memeTitleText.innerHTML = title;
-//     memeImg.setAttribute("src", url);
-//     memeAuthorText.innerHTML = `Meme by: ${author}`;
-// }
-//* neo:
 function updateDetails(title, url) {
     memeTitleText.innerHTML = title;
     memeImg.setAttribute("src", url);
-    // memeAuthorText.innerHTML = `Meme by: ${author}`;
 }
 
 
@@ -25,16 +18,6 @@ function errorHandler(error) {
 
 
 function generateMeme() {
-    // fetch("https://meme-api.com/gimme/wholesomememes")
-    // .then((response) => {
-    //     response.json();
-    // })
-    // .then((data) => {
-    //     updateDetails(data.title, data.url, data.author);
-    // })
-    // .catch(errorHandler)
-
-    //* neo:
     fetch("https://vast-puce-mite-fez.cyclic.app/animeme")
     .then((response) => {
         response.json();
